f6a013049b903fca556ebb9a940cba93925a85f9
crivasf <zionsdo@gmail.com>
2024-05-18 14:37:16 -0600

    cambiar database_config_pg

--- src/configuraciones/env.conexion.js
+++ src/configuraciones/env.conexion.js
@@ -1,16 +1,11 @@
 export default {
    "conexion": {
       "database_config_pg": {
-         "user": "usrAdmin",
-         "password": "D0ki1ohsg1",
-         "database": "master",
-         "server": "172.36.0.11",
-         "port": 1433,
-         "max": 10,
-         "min": 0,
-         "idleTimeoutMillis": 30000
-      
-         
+         "host": "localhost",
+         "database": "whatsbotsm",
+         "user": "crivasf",
+         "password": "cr1v4sf2023",
+         "port": 5432
       }
    },
    "SignatureCer": "00000100000199901097.pfx",
